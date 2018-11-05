3268c5b40321c3a46bb0e16ee431e8b52a7214f2
Jared Paquette <jpaquett@c28-0113-33.ad.mtu.edu>
2018-11-05 14:34:15 -0500

    test upload

--- src/main/logIn.php
+++ src/main/logIn.php
@@ -6,23 +6,30 @@ $dbh = mysql_connect("classdb.it.mtu.edu:3307", "jpaquett", "squidward")
 $db  = mysql_select_db("tspdatabase", $dbh)
         or die ("Couldn't select database.");
 
-$sql = "SELECT username FROM user";
+$sql = "SELECT password , username FROM user where username = '".$_POST['username']."';";
 
 $result = mysql_query($sql, $dbh)
         or die("SQL statement is wrong.");
 
-while ($row = mysql_fetch_array($result)) {
-    $username = $row['username'];
-    //$password = $row['password'];
-    //echo '<br> .$username. </br>'
-    //echo '<br> .$password. </br>'
+//echo mysql_error($result);
+$row = mysql_fetch_array($result);
+//echo 'password'".$row['password']."'';
+//echo ' username'".$row['username']."'';
+
+if ( str_rot13($row[0] == $_POST['password']) && ($row[1] == $_POST['username']) ) {
+    echo 'reached if';        
+    header("LOCATION:conlander.php");
+        
+}
+else {
+    echo 'Wrong username or password';
 }
 
 mysql_close($dbh);
 
-echo"<h1>Calendar Sign In</h1>";                                                                                                       
-echo'<form method = "post" action = "conlander.php ">';                                           
-echo'Email: <input type = "username"  name = "username"><br/>';                                      
-echo'Password: <input type = "password" name = "password"><br/>';                                    
-echo'<input type = "submit" value = "Submit">';   
+//echo"<h1>Calendar Sign In</h1>";                                                                                                       
+//echo'<form method = "post" action = "conlander.php ">';                                           
+//echo'Email: <input type = "username"  name = "username"><br/>';                                      
+//echo'Password: <input type = "password" name = "password"><br/>';                                    
+//echo'<input type = "submit" value = "Submit">';   
 ?>
