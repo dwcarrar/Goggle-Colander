082e9100aad7f837264de8bad42e991957fe3786
Jared Paquette <jpaquett@c28-0113-24.ad.mtu.edu>
2018-11-08 21:37:10 -0500

    fixed the login

--- src/main/logIn.php
+++ src/main/logIn.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+if (isset($_POST["username"])) {
 
 $dbh = mysql_connect("classdb.it.mtu.edu:3307", "jpaquett", "squidward")
         or die ("Couldn't connect to database.");
@@ -11,12 +13,16 @@ $sql = "SELECT password , username FROM user where username = '".$_POST['usernam
 $result = mysql_query($sql, $dbh)
         or die("SQL statement is wrong.");
 
-//echo mysql_error($result);
 $row = mysql_fetch_array($result);
-//echo 'password'".$row['password']."'';
-//echo ' username'".$row['username']."'';
 
-if ( str_rot13($row[0] == $_POST['password']) && ($row[1] == $_POST['username']) ) {
+
+if ( $_POST["username"] == ""  ) {
+
+    echo 'Please enter a password';
+    header("LOCATION:home.php");
+
+}
+else if ( ((str_rot13($row[0]) == $_POST['password']) && ($row[1]) == $_POST['username']) ) {
     echo 'reached if';        
     header("LOCATION:conlander.php");
         
@@ -24,12 +30,7 @@ if ( str_rot13($row[0] == $_POST['password']) && ($row[1] == $_POST['username'])
 else {
     echo 'Wrong username or password';
 }
-
+}
 mysql_close($dbh);
 
-//echo"<h1>Calendar Sign In</h1>";                                                                                                       
-//echo'<form method = "post" action = "conlander.php ">';                                           
-//echo'Email: <input type = "username"  name = "username"><br/>';                                      
-//echo'Password: <input type = "password" name = "password"><br/>';                                    
-//echo'<input type = "submit" value = "Submit">';   
 ?>
