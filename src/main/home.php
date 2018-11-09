27f88a103e9541d10ae24f63294e20f30762290f
Daniel Carrara <dwcarrar@guardian.it.mtu.edu>
2018-11-08 23:53:00 -0500

    home update/signUp rework

--- src/main/home.php
+++ src/main/home.php
@@ -1,26 +1,62 @@
 <?php
-    echo"<h1>Goggle Colander</h1>";
+    include 'signUp.php'; 
+        
+    if (isset($_POST['SignSubmit'])) {
+        $name = htmlentities($_POST['name']);
+        $uname = htmlentities($_POST['susername']);
+        $pass1 = htmlentities($_POST['password1']);
+        $pass2 = htmlentities($_POST['password2']);
+ 
+        $signer = new signUp();
+        $signer->TrySignUp($name,$uname,$pass1,$pass2);
+    }
 ?>
 <html>
 
+<style>
+    body {
+        background-color: powderblue;
+    }
+
+    form {
+        text-align: center;
+        margin: auto;
+        width: 60%;
+        padding: 10px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    .img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+
+</style>
+
 <body>
 
-<p>
-<form method = post action = logIn.php>
-Username: <input type="username" name="username"></br>
-Password: <input type="password" name="password"></br>
-<input type = "submit" value = "Sign In">
-</form>
-</p>
+<h1 style="color:blue;"> Goggle Colander </h1>
 
-<p>
-<form method = "post" action = "signUp.php">
-Name: <input type="username" name="name"></br>
+<form class="center" method = post action = "logIn.php">
 Username: <input type="username" name="username"></br>
 Password: <input type="password" name="password"></br>
-<input type = "submit" value = "Sign Up">
+<input type = "submit" name = "LogSubmit" value = "Sign In">
 </form>
-</p>
+
+<form method = "post" action = "">
+Name: <input type="username" name="name"></br>
+Username: <input type="username" name="susername"></br>
+Password: <input type="password" name="password1"></br>
+Re-Enter Your Password: <input type="password" name="password2"></br>
+<input type = "submit" name = "SignSubmit" value = "Sign Up">
+</form>
+
+<img src="logo.png" align="middle" class="img">
 
 </body>
 
--- src/main/signUp.php
+++ src/main/signUp.php
@@ -1,29 +1,54 @@
 <?php
-
-//try {
-    //$config = parse_ini_file("db.ini");
-    //$dbh = new PDO("classdb.it.mtu.edu:3307", "jpaquett", "squidward");
-    //$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh = mysql_connect("classdb.it.mtu.edu:3307", "jpaquett", "squidward")                             
-            or die ("Couldn't connect to database.");                                                    
-                                                                                                  
-    $db  = mysql_select_db("tspdatabase", $dbh)                                                          
-            or die ("Couldn't select database.");                                                        
-    $actPass = str_rot13($_POST['password']);
-    $sql = "insert into user( name, username, password) values ('".$_POST['name']."', '".$_POST['username']."', '$actPass');";
+class signUp {
     
-    $result = mysql_query( $sql, $dbh)
-               or die("SQL statement is wrong.");
+    //Verifies a valid username and matching passwords and creates new account
+    function TrySignUp ($name, $uname, $pass1, $pass2) {                                                    
+        $dbh = mysql_connect("classdb.it.mtu.edu:3307", "jpaquett", "squidward")                          
+            or die ("Couldn't connect to database.");                                                 
+        $db  = mysql_select_db("tspdatabase", $dbh)                                                       
+            or die ("Couldn't select database.");
 
-    echo mysql_error($dbh); 
-    //$statement = $dbh->prepare( "insert into user( name, username, password) values :name, :username, :password");
-    //$actPass = str_rot13($_POST['password']);
-    //$result = $statement->execute(array(':name'=>$_POST['name'], ':username'=>$_POST['username'], ':name'=>$actPass));
-    header("LOCATION:conlander.php");
-    mysql_close($dbh);
-//}
-//catch (PDOException $e) {
-//    print " Failed to register for Goggle Colander " .$e->getMessage()." <br/>";
-//    die();
-//}
+        $unique = $this->UniqueName($uname);
+        if (!($unique)) {
+            echo "This username is already in use.";             
+            header("LOCATION:home.php");
+        }
+        else if ($pass1 != $pass2) {
+            echo "The passwords entered do not match!";
+            header("LOCATION:home.php");
+        }
+        else {
+            $actPass = str_rot13($pass1);
+            $sql = "insert into user(name, username, password) 
+		       values ('$name', '$uname', '$actPass');";
+    
+            $result = mysql_query( $sql, $dbh)
+                   or die("SQL statement is wrong.");
+
+            echo mysql_error($dbh); 
+            header("LOCATION:conlander.php");
+        }
+        mysql_close($dbh);
+    }
+
+    //verifies that the username is not already in the database
+    function UniqueName ($uname) {
+        $dbh = mysql_connect("classdb.it.mtu.edu:3307", "jpaquett", "squidward", true)                          
+            or die ("Couldn't connect to database.");                                                 
+        $db  = mysql_select_db("tspdatabase", $dbh)                                                       
+            or die ("Couldn't select database.");
+
+        $sql = "select username from user where username = '$uname'";
+        $name_occurs = mysql_num_rows(mysql_query($sql, $dbh));
+        mysql_close($dbh);
+
+        if ($name_occurs == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
 ?>
