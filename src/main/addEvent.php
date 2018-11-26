418bf4b7273a87c01bbdf6850c9dc5a13f46b84b
Jared Paquette <jpaquett@c28-0112-15.ad.mtu.edu>
2018-11-25 19:45:59 -0500

    Add event code

--- src/main/addEvent.php
+++ src/main/addEvent.php
@@ -1,14 +1,19 @@
 <?php
 class Event {
 
-    function AddEvent ( $eventID, $name, $description, $start, $end ) {
+    function AddEvent ( $name, $description, $start, $end ) {
     
         $dbh = mysql_connect("classdb.it.mtu.edu:3307", "jpaquett", "squidward", true) 
                 or die ("Couldn't connect to database.");                                  
         $db  = mysql_select_db("tspdatabase", $dbh)                                    
                 or die ("Couldn't select database.");                                      
         
-        
+        $sql = "INSERT into event(name, description, startTime, endTime) values('$name', '$description', '$start', '$end');";
+
+        $result = mysql_query($sql, $dbh)
+            or die ("SQL statement is wrong.");
+
+        mysql_close($dbh);
     
     }
 }
