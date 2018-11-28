85136264d076aabf65b13ec94337262d9eed3417
Daniel Carrara <dwcarrar@c28-0113-14.ad.mtu.edu>
2018-11-27 21:50:14 -0500

    start of event functionality

--- src/main/conlander.php
+++ src/main/conlander.php
@@ -1,6 +1,22 @@
  <?php
    date_default_timezone_set('America/Detroit');
-    
+   
+        include 'addEvent.php';
+ 
+        if (isset($_POST['submitDate'])) {
+            $date = htmlentities($_POST['date']);
+        } 
+        if (isset($_POST['submit']) && isset($_POST['name'])) {
+                $name = htmlentities($_POST['name']);
+                $desc = htmlentities($_POST['description']);
+                $start = htmlentities($_POST['startTime']);
+                $end = htmlentities($_POST['endTime']);
+
+                $event = new Event();
+                //$event->AddEvent($name,$desc,$date,$start,$end);
+        }
+
+ 
     if (isset($_GET['ym'])){
         $ym = $_GET['ym'];
     } else {
@@ -32,10 +48,15 @@
         $date = $ym.'-'.$day;
 
         if ($today == $date) {
-            $week .= '<td class="today">'.$day;
+            $week .= '<td class="today"><form method="post" action="#modal">';
+            $week .= '<input type="hidden" name="date" value="'.$date.'">
+                          <input class="dateEvents" type="submit" name="submitDate" value="'.$day.'"></form>';
+
         }
         else {
-            $week .= '<td>'.$day; 
+            $week .= '<td><form method="post" action="#modal">';
+            $week .= '<input type="hidden" name="date" value="'.$date.'">
+                          <input class="dateEvents" type="submit" name="submitDate" value="'.$day.'"></form>';
         }
         $week .= '</td>'; 
 
@@ -70,6 +91,7 @@
         }
         td {
             height: 100px;
+            width: 150px;
         }
         .today {
             background: #fffd8e;
@@ -79,6 +101,82 @@
         }
         th:nth-of-type(1),td:nth-of-type(1){
             color: red;
+        }
+        .dateEvents {
+            position:relative;
+            top: -10px;
+            left: -10px;
+            width: 35px;
+            height: 35px;
+            text-align:center;
+            border:none;
+            background: rgba(255,255,255,0);
+            opacity= .5;
+        }
+        .dateEvents:hover {
+            cursor:pointer;
+            font-weight: bold;
+        }        
+        .modal_container {
+            width:100%;
+            height:100%;
+            position:fixed;
+            top:0px;
+            background:rgba(0,130,180,.8);
+            opacity:0;
+            pointer-events: none;
+            transition: all 500ms ease;
+        }
+        .modal_container:target{
+            opacity:1;
+            pointer-events: auto;
+        }
+       
+        .modal{
+            display: block;
+            width:350px;
+            height:400px;
+            margin:auto;
+            position: absolute;
+            top:0px; bottom:0px;
+            right:0px; left:0px;
+            background: powderblue;
+            border:2px solid #222;
+            padding:20px;
+            box-shadow: 0px 0px 50px 5px black;
+        }
+
+        .btnSubmit{
+            background:green;
+            color:white;
+            margin-top:10px;
+            border-radius:5px;
+        }
+        .btnSubmit:hover{
+            background:#b80000;
+        }
+        .modal_heading{
+            text-align:center;
+            font-family: big john;
+            font-size:20pt;
+        }
+        .close{
+            color:white;
+            position: absolute;
+            border: 2px solid #333;
+            padding:7px 9px 11px 9px;
+            font-family: big john;
+            background:red;
+            opacity:1;
+            text-decoration:none;
+            top:0px;
+            right:0px;
+            border-radius: 5px;
+            transition: background 500ms; 
+        }
+        .close:hover{
+            background:#444;
+            cursor:pointer;
         }
     </style>
 </head>
@@ -104,5 +202,21 @@
             ?>           
         </table>
     </div>
+    <div class="modal_container" id="modal">
+        <div class="modal">
+            <a href="#" class="close">X</a>
+            <span class="modal_heading">
+                CREATE EVENT
+            </span>
+            <form method="post" action="#">
+                <input type="text" placeholder="Name" name="name"></br>
+                <input type="text" placeholder="Description" name="description"></br>
+                Start Time: <input type="time" name="startTime"></br>
+                End Time: &nbsp<input type="time" name="endTime"></br>
+                <input type = "submit" class="btnSubmit" name = "submit" value = "Submit">
+            </form>
+        </div>
+    </div>
+
 </body>
 </html>
